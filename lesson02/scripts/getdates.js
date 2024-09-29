7899bbca1aa0aac8d53fb62465b95223df98ba47
elabanca <elabanca@byui.edu>
2024-09-28 19:24:14 -0600

    completed design assignment

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,6 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text;
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentYear").innerHTML = year + "&copy;<br> Hared Albancando Robles<br> ID, USA";
