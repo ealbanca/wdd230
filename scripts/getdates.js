d73bdfa22c126550d45c6796d68509fdd9067f0c
elabanca <elabanca@byui.edu>
2024-10-04 21:19:24 -0600

    Added map API

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ document.getElementById("lastModified").innerHTML = "Last Modification: " + text
 
 const d = new Date();
 let year = d.getFullYear();
-document.getElementById("currentYear").innerHTML = year + "&copy;<br> Hared Albancando Robles<br> ID, USA";
+document.getElementById("currentYear").innerHTML = year + "&copy;<br> Hared Albancando Robles<br> Bogota, Colombia";
