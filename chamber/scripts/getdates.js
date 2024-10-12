1c0b057484cc1887f466d564844ebbebe2dea890
elabanca <elabanca@byui.edu>
2024-10-12 14:32:34 -0600

    Updated cahmber home page

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,6 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + text + "<br><br>Project for WDD 230";
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentYear").innerHTML = year + "&copy;<br><br> Hared Albancando Robles";
