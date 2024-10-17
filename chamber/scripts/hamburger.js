a900882da80310ad471baaf99d62bea07d9402f1
elabanca <elabanca@byui.edu>
2024-10-16 20:58:41 -0600

    Updated Dark mode

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', function () {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+// Dark Mode
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
