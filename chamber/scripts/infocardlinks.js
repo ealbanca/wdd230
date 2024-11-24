9deef6108873fc76d3725bbbaea181a2111e0fe8
elabanca <elabanca@byui.edu>
2024-11-23 17:54:24 -0700

    Updated

--- chamber/scripts/infocardlinks.js
+++ chamber/scripts/infocardlinks.js
@@ -1,4 +1,4 @@
-const baseURL = "https://ealbanca.github.io/wdd230/chamber";
+const baseURL = "https://ealbanca.github.io/wdd230/";
 const linksURL = "https://ealbanca.github.io/wdd230/chamber/data/infocard.json";
 
 async function getLinks() {
